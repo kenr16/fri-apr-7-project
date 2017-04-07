95cdd82c1abeb23a346513061c8d2ba300e2844f
Ken Rutan <kenr16@gmail.com>
2017-04-07 11:04:14 -0700

    Add the ability to order multiple pizzas, as well as the ability to take multiple inputs from multiple checkbox forms.

--- js/scripts.js
+++ js/scripts.js
@@ -31,20 +31,25 @@ function Customer(fullName, street, city, state, zip) {
 // **********************************************************
 $(document).ready(function() {
 
-  $("form#new-pizza").submit(function(event) {
+  $("form#all-pizzas").submit(function(event) {
     event.preventDefault();
-      //var pizzaSizeInput = $(this).find("#addressType").val();
-      var pizzaSizeInput = document.getElementById('pizzaSize').value;
-      var pizzaBaseInput = document.getElementById('pizzaBase').value;
+    //var pizzaSizeInput = $(this).find("#addressType").val();
+    //var pizzaSizeInput = document.getElementById('pizzaSize').value;
+    //var pizzaBaseInput = document.getElementById('pizzaBase').value;
+    //var newPizza = new Pizza(pizzaSizeInput, pizzaBaseInput);
+
+    $(".new-pizza").each(function() {
+      var pizzaSizeInput = $(this).find("#pizzaSize").val();
+      var pizzaBaseInput = $(this).find("#pizzaBase").val();
       var newPizza = new Pizza(pizzaSizeInput, pizzaBaseInput);
-      $("input:checkbox[name=toppings-availible]:checked").each(function(){
+      $(this).find("input:checkbox[name=toppings-availible]:checked").each(function(){
         var pizzaToppingsInput = $(this).val();
         newPizza.addTopping(pizzaToppingsInput);
       });
+
       alert(newPizza.toppings);
-      alert(newPizza.cost());
-      //$("#work-responses").show();
-      //$('#transportation_survey').hide();
+
+    });
   });
 
   $("form#new-customer").submit(function(event) {
@@ -58,6 +63,61 @@ $(document).ready(function() {
     alert(newCustomer.fullName);
 
   });
+
+  $("#add-pizzas").click(function() {
+    $("#addedPizzas").append(
+      '<hr>' +
+      '<h2>Next Pizza:</h2>' +
+      '<div class="new-pizza">' +
+        '<label for="pizzaSize">Select the size of your pizza:</label>' +
+        '<select class="form-control" id="pizzaSize">' +
+          '<option value="8">Personal (8")</option>' +
+          '<option value="10">Small (10")</option>' +
+          '<option value="12">Medium (12")</option>' +
+          '<option value="14">Large (14")</option>' +
+          '<option value="16">XtraLarge (16")</option>' +
+          '<option value="18">New York (18")</option>' +
+        '</select>' +
+        '<label for="pizzaBase">Select a base for your pizza:</label>' +
+        '<select class="form-control" id="pizzaBase">' +
+          '<option value="Tomato">Tomato</option>' +
+          '<option value="Pesto">Pesto</option>' +
+        '</select>' +
+        '<div class="form-group">' +
+          '<div class="row">' +
+            '<p><b>Please select which toppings you would like added to your pizza:</b></p>' +
+            '<div class="col-xs-5">' +
+              '<input type="checkbox" name="toppings-availible" value="Cheese">Cheesee ($0.50)<br>' +
+              '<input type="checkbox" name="toppings-availible" value="Double Cheese">Double Cheese  ($0.50)<br>' +
+              '<input type="checkbox" name="toppings-availible" value="Triple Cheese">Triple Cheese  ($0.50)<br>' +
+              '<input type="checkbox" name="toppings-availible" value="Pepperoni">Pepperoni  ($0.50)<br>' +
+            '</div>' +
+            '<div class="col-xs-5">' +
+              '<input type="checkbox" name="toppings-availible" value="Sausage">Sausage  ($0.50)<br>' +
+              '<input type="checkbox" name="toppings-availible" value="Bacon">Bacon  ($0.50)<br>' +
+              '<input type="checkbox" name="toppings-availible" value="Mushrooms">Mushroom ($0.50)<br>' +
+              '<input type="checkbox" name="toppings-availible" value="Onion">Onion  ($0.50)<br>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>'  );
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
